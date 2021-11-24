fd50e27896ad7fdeca050ffbe0338dbd65a9b78d
yasudamasaharu <mshr-0926@ur.ttn.ne.jp>
2021-11-24 12:18:51 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import _ from 'lodash'
+import { NAME as NAME_OF_MY } from './utilities'
+import Lion from './utilities'
+
+
+
+console.log(Lion.say());
+//console.log(utilities.Nijou(3));
+console.log(NAME_OF_MY);
 
 function component(){
   const element = document.createElement('div');
@@ -8,3 +16,6 @@ function component(){
 }
 
 document.body.appendChild(component());
+
+
+
---
+++ src/utilities.js
@@ -0,0 +1,15 @@
+export function Nijou(num) {
+  return num ** 2;
+}
+
+
+export const NAME = 'My'
+
+
+export default class Lion {
+  static say() {
+      return 'Roar'
+   }
+}
+
+
