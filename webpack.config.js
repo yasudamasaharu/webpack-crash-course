cefeedc722ad7a4520aa22274e8a564b4aa0c85e
yasudamasaharu <mshr-0926@ur.ttn.ne.jp>
2021-11-24 11:14:13 +0900

    install webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const path = require('path');
+const path = require('path')
 
 const outputPath = path.resolve(__dirname, 'dist')
 
-module.export = { 
+module.exports = { 
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: outputPath 
+  },
+  devServer: {
+     contentBase: outputPath
   }
 }
-
-
-
